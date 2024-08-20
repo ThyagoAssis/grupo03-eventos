13c88f1514f66bd163284e08680ddbd4d63bf1a8
Thyago Assis <thyagoalmeidaprof@gmail.com>
2024-08-20 14:00:27 -0300

    Corrigido

--- agenda_eventos/urls.py
+++ agenda_eventos/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
 from lista.views import EventoListView, sobre
 
 
--- lista/models.py
+++ lista/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Evento(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
-    data2 = models.DateField(null=False)
+    data = models.DateField(default='DD-MM-YYYY',null=True, blank=True)
+    hora = models.TimeField(default=datetime.time(9, 0),null=True, blank=True)
     numero_convidados = models.CharField(max_length=30,null=False, blank=False)
     local = models.CharField(max_length=100,null=False, blank=False)
